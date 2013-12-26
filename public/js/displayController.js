7da10e405e3a54fce6b527447e2e50388359be21
OpenShift guest <harryphan@gmail.com>
2013-12-26 13:52:57 -0500

    UI work

--- public/js/displayController.js
+++ public/js/displayController.js
@@ -1,7 +1,13 @@
 define('displayController',['jquery'],function($){
+    $(document).bind('appendThought',function(event){
+        var val = event.detail
+        $('#thoughts').append('<div class="thought">'+JSON.stringify(val.question.content)+'</div>');
+    });
     $(document).bind('loadThoughts',function(event){
         $('#thoughts').empty();
-        $('#thoughts').append('<span>'+JSON.stringify(event.detail)+'</span>');
+        $.each(event.detail,function(key, val){
+            $(document).trigger({type:'appendThought', detail:val});
+        });
     });    
     
 })
